5fa020170693afb0eb8e15644a4b366b5fbbb9a1
ANUSHKA SHAH <anushkashah@ANUSHKAs-Laptop.local>
2025-11-20 21:57:51 +0530

    Fix map null error for Vercel deployment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "babe66579a820aee"; }
+/******/ 		__webpack_require__.h = function() { return "82441031cc7abdc5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
